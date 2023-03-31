925668606272cb999bc4e28e23c68fdbb58e5c07
AnthonyNet <zejda.antonin@gmail.com>
2023-03-31 08:34:28 +0200

    button props-switch

--- src/components/card/Button.svelte
+++ src/components/card/Button.svelte
@@ -1,11 +1,30 @@
 <script>
-	const style= {
-		button: "px-6 py-2.5 mt-2 mb-2 bg-blue-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-indigo-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-	}
+	const style = {
+		button:
+			'px-6 py-2.5 mt-2 mb-2 bg-blue-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-indigo-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out',
+		ul: 'flex  justify between items-center'
+	};
+
+	let props = {
+		base: 'base',
+		pastSimple: 'pastSimple',
+		pastParticiple: 'pastParticiple'
+	};
 	// your script goes here
+	let switchState = false;
+	console.log(switchState);
+	function toggleSwitch() {
+		switchState = !switchState;
+	}
 </script>
 
 <div>
-	<button class={style.button}>hint</button>
-	<ul></ul>
+	<button class={style.button} on:click={toggleSwitch}>hint</button>
+	<ul class={style.ul}>
+		{#if switchState}
+			<li>{props.base}</li>
+			<li>{props.pastSimple}</li>
+			<li>{props.pastParticiple}</li>
+		{/if}
+	</ul>
 </div>
