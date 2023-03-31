6f79fec4f9e34b99ff141de40cb89bb55882aa1e
AnthonyNet <zejda.antonin@gmail.com>
2023-03-31 15:48:40 +0200

    read-only irregular added

--- src/components/card/Input.svelte
+++ src/components/card/Input.svelte
@@ -1,8 +1,8 @@
 <script context="module">
 const style = {
-	li: "flex flex-col items-center",
+	li: "flex flex-col items-center ",
 	li_right: "bg-green-500",
-	input: "border-2 py-1 m-2 text-center w-full"
+	input: "border-2 py-1 m-2 text-center w-full font-extrabold",
 }
 </script>
 
@@ -12,8 +12,10 @@ const style = {
     export let props:any;
 	export let definition:string = '';
 
+	console.log(props);
+
     </script>
 // check value in input
    <li class={word===props?style.li + " " + style.li_right:null}>
-        <input type="text" class={style.input} placeholder={definition} bind:value={word}/>
+        <input type="text" class={word===props? style.input + " text-green-500": style.input + " text-red-500"} placeholder={definition} bind:value={word} readonly={word===props?true:false}/>
     </li>
