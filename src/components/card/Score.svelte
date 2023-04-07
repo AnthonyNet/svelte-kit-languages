320a73e74e286b80681af7dd4ef1d0359f6efed4
AnthonyNet <zejda.antonin@gmail.com>
2023-04-07 13:26:00 +0200

    formatter

--- src/components/card/Score.svelte
+++ src/components/card/Score.svelte
@@ -1,24 +1,23 @@
 <script>
-  import { onMount } from 'svelte';
-  import { writable } from 'svelte/store';
+	import { onMount } from 'svelte';
+	import { writable } from 'svelte/store';
 
-  const totalScore = writable(0);
+	const totalScore = writable(0);
 
-  onMount(() => {
-    const storedScore = localStorage.getItem('totalScore');
-    if (storedScore) {
-      totalScore.set(parseInt(storedScore));
-    } else {
-      localStorage.setItem('totalScore', '0');
-    }
-  });
-// check the function increaseScore and fix the error
+	onMount(() => {
+		const storedScore = localStorage.getItem('totalScore');
+		if (storedScore) {
+			totalScore.set(parseInt(storedScore));
+		} else {
+			localStorage.setItem('totalScore', '0');
+		}
+	});
+	// check the function increaseScore and fix the error
 
-
-  function increaseScore() {
-    totalScore.update(score => score + 1);
-    localStorage.setItem('totalScore', totalScore.toString());
-  }
+	function increaseScore() {
+		totalScore.update((score) => score + 1);
+		localStorage.setItem('totalScore', totalScore.toString());
+	}
 </script>
 
 <span>{$totalScore}</span>
