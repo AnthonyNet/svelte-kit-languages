69ef3ad9fd568371ab0c22e895c253952ad1b5ad
AnthonyNet <zejda.antonin@gmail.com>
2023-04-08 13:26:19 +0200

    formatter

--- src/components/card/Star.svelte
+++ src/components/card/Star.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-    import Icon from '@iconify/svelte';
-    export let stars: number[] | [] = [0];
-
+	import Icon from '@iconify/svelte';
+	export let stars: number[] | [] = [0];
 </script>
 
 {#each stars as star}
-
-<Icon icon="material-symbols:star" />
+	<Icon icon="material-symbols:star" />
 {/each}
